f5c26c845d64c222df97c53afb1f67b843ff07b4
Aleksandar Stankovic <alxnaruto@gmail.com>
2015-12-01 23:56:57 +0100

    Init

---
+++ README.md
@@ -0,0 +1,3 @@
+Software 'noggin' for College developed by:
+- Filip Bekić
+- Aleksandar Stanković
---
+++ src/main/java/models/File.java
@@ -0,0 +1,44 @@
+package models;
+
+public class File {
+	
+	private Integer id;
+	private String fileName;
+	private String MIME;
+	
+	public File(){}
+
+	public File(Integer id, String fileName, String mIME) {
+		super();
+		this.id = id;
+		this.fileName = fileName;
+		MIME = mIME;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getFileName() {
+		return fileName;
+	}
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+
+	public String getMIME() {
+		return MIME;
+	}
+
+	public void setMIME(String mIME) {
+		MIME = mIME;
+	}
+	
+	
+	
+}
