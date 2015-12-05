21cc6c036627864c756117e0d5a8119dcc6b89c2
alxbxbx <acastankovic.bb@gmail.com>
2015-12-05 17:07:58 +0100

    Database structure changed.

--- noggin.sql
+++ noggin.sql
@@ -22,7 +22,7 @@
 DROP TABLE IF EXISTS `categoy`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `categoy` (
+CREATE TABLE `category` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(45) NOT NULL,
   PRIMARY KEY (`id`),
--- src/main/java/com/noggin/rest/FileController.java
+++ src/main/java/com/noggin/rest/FileController.java
@@ -2,6 +2,7 @@ package com.noggin.rest;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,15 +11,21 @@ import org.springframework.web.bind.annotation.RestController;
 import com.noggin.models.File;
 
 @RestController
+@RequestMapping("file")
 public class FileController {
 	
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 	
-	@RequestMapping("/file")
-	public File file(@RequestParam(value="name", defaultValue="World") String name){
-		return new File((int) counter.incrementAndGet(),
-                String.format(template, name), "Come again!");
+	File file = new File();
+	
+	@RequestMapping(value = "/{name}", method = RequestMethod.GET, produces = "application/json")
+	public File file(@PathVariable String name){	
+		file.setFileName(name);
+		file.setMIME("New Mime");
+		file.setId(1);
+		
+		return file;
 		
 	}
 
