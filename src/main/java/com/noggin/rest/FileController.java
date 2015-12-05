10e6181c1a3f5ce657423531c903e45da1452517
alxbxbx <acastankovic.bb@gmail.com>
2015-12-05 01:00:48 +0100

    Beggining with Rest controllers

---
+++ src/main/java/com/noggin/rest/FileController.java
@@ -0,0 +1,25 @@
+package com.noggin.rest;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.noggin.models.File;
+
+@RestController
+public class FileController {
+	
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+	
+	@RequestMapping(value="/file", method=RequestMethod.GET)
+	public File file(@RequestParam(value="name", defaultValue="World") String name){
+		return new File((int) counter.incrementAndGet(),
+                String.format(template, name), "Come again!");
+		
+	}
+
+}
