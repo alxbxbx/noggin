b8c61486a17b937b15c354fd6d7f972c82e45f4a
Filip Bekic <filip1994sm@gmail.com>
2015-12-09 18:59:05 +0100

    Fixed persistance config import

--- src/main/java/com/noggin/spring/PersistenceConfig.java
+++ src/main/java/com/noggin/spring/PersistenceConfig.java
@@ -1,8 +1,5 @@
 package com.noggin.spring;
-import javax.persistence.EntityManagerFactory;
-import javax.sql.DataSource;
-
-import org.apache.tomcat.dbcp.dbcp.BasicDataSource;
+import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +9,9 @@ import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
 
 @Configuration
 @EnableJpaRepositories("com.noggin.dao.repositories")
