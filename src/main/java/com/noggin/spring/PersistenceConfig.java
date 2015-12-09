3048cfe738b1e2d46144c4290e1d8b650d6ef1d8
alxbxbx <acastankovic.bb@gmail.com>
2015-12-09 19:02:44 +0100

    Persistance udpate

--- src/main/java/com/noggin/spring/PersistenceConfig.java
+++ src/main/java/com/noggin/spring/PersistenceConfig.java
@@ -1,4 +1,7 @@
 package com.noggin.spring;
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
 import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -9,9 +12,6 @@ import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import javax.persistence.EntityManagerFactory;
-import javax.sql.DataSource;
 
 @Configuration
 @EnableJpaRepositories("com.noggin.dao.repositories")
