a855cc1c2f04d7cf580e4f11321b787ded8bd23d
alxbxbx <acastankovic.bb@gmail.com>
2015-12-19 21:24:57 +0100

    PersistenceConfig now works with application.properties

--- src/main/java/com/noggin/dao/config/PersistenceConfig.java
+++ src/main/java/com/noggin/dao/config/PersistenceConfig.java
@@ -2,9 +2,12 @@ package com.noggin.dao.config;
 
 import org.apache.commons.dbcp.BasicDataSource;
 import org.hibernate.ejb.HibernatePersistence;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
@@ -20,15 +23,19 @@ import javax.sql.DataSource;
 @EnableJpaRepositories({"com.noggin.dao.repositories"})
 @EnableTransactionManagement
 @ComponentScan({"com.noggin"})
+@PropertySource({ "classpath:application.properties" })
 public class PersistenceConfig {
+	
+	@Autowired
+	Environment env;
 	
 	  @Bean
 	  public DataSource dataSource() {
 		  BasicDataSource dataSource = new BasicDataSource();
-	      dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-	      dataSource.setUrl("jdbc:mysql://localhost/noggin");
-	      dataSource.setUsername("root");
-	      dataSource.setPassword("");
+	      dataSource.setDriverClassName(env.getProperty("jdbc.driverClassName"));
+	      dataSource.setUrl(env.getProperty("jdbc.url"));
+	      dataSource.setUsername(env.getProperty("jdbc.user"));
+	      dataSource.setPassword(env.getProperty("jdbc.pass"));
 	      return dataSource;
 	  }
 	  
--- src/main/java/com/noggin/rest/CategoryController.java
+++ src/main/java/com/noggin/rest/CategoryController.java
@@ -1,16 +1,13 @@
 package com.noggin.rest;
 
-import com.noggin.dao.config.PersistenceConfig;
 import com.noggin.dao.repositories.ICategory;
 import com.noggin.models.Category;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
