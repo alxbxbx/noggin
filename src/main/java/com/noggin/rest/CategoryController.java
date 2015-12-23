5003c153b99b08e4e61529ddcd8307fcd9bab7c8
alxbxbx <acastankovic.bb@gmail.com>
2015-12-23 19:36:13 +0100

    Put controller for Category

--- src/main/java/com/noggin/rest/CategoryController.java
+++ src/main/java/com/noggin/rest/CategoryController.java
@@ -4,6 +4,10 @@ import com.noggin.dao.repositories.ICategory;
 import com.noggin.models.Category;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +25,20 @@ public class CategoryController {
         List<Category> list = ic.findAll();
        
         return list;
+    }
+    @RequestMapping(value= "/category/{id}", method = RequestMethod.PUT, produces = "application/json")
+    public ResponseEntity<Category> update(@PathVariable String id, @RequestBody Category category){
+    	Category cat = new Category();
+    	Integer intId = null;
+    	try{
+    		intId = Integer.parseInt(id);
+    		cat = ic.getOne(intId);
+    	}catch (Exception e){
+    		return new ResponseEntity<Category>(HttpStatus.NOT_FOUND);
+    	}
+    	cat.setName(category.getName());
+    	ic.save(cat);
+    	return new ResponseEntity<Category>(cat,HttpStatus.OK);
     }
 
 }
