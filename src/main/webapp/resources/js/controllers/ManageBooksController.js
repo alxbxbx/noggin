fec48d4a0bcc9cf54ebc69d43306462089096b41
Filip Bekic <filip1994sm@gmail.com>
2016-01-02 17:55:10 +0100

    Update book works perfectly.

--- src/main/webapp/resources/js/controllers/ManageBooksController.js
+++ src/main/webapp/resources/js/controllers/ManageBooksController.js
@@ -1,33 +1,48 @@
 'use strict';
 
-app.controller('ManageBooksController', ['$scope', '$http', 'bookFactory', function ($scope, $http, bookFactory) {
+app.controller('ManageBooksController', ['$scope', '$http', 'bookFactory', 'languageFactory', 'categoryFactory',
+    function ($scope, $http, bookFactory, languageFactory, categoryFactory) {
 
-    $scope.book = {};
-
+    // Get Books
     bookFactory.getAll().success(function(data) {
         $scope.books = data;
     });
 
-    $scope.clickEditBook = function(id, title, author, keywords, publicationYear) {
-        $scope.book.id = id;
-        $scope.book.title = title;
-        $scope.book.author = author;
-        $scope.book.keywords = keywords;
-        $scope.book.publicationYear = publicationYear;
+    // Get Categories
+    languageFactory.getAll().success(function(data) {
+        $scope.languages = data;
+    });
+
+    // Get Languages
+    categoryFactory.getAll().success(function(data) {
+        $scope.categories = data;
+    });
+
+    // Select Book
+    $scope.clickEditBook = function(bookByReference) {
+        $scope.book = bookByReference;
+        console.log("Selected book:");
+        console.log(bookByReference);
     };
 
+    // Save Book
     $scope.clickUpdateBook = function() {
         if (!$scope.book.title || !$scope.book.keywords) {
             alert("Title and keywords are required!");
         } else {
+            console.log("Preparing to update book:");
+            console.log($scope.book);
             $http.put('/book/' + $scope.book.id, $scope.book).success(function (data) {
                 bookFactory.getAll().success(function(data) {
                     $scope.books = data;
                 });
+                console.log("Book updated successfully.");
+                $('#editBookModal').modal('hide');
             });
         }
     };
 
+    // Remove Book
     $scope.clickRemoveBook = function() {
 
     };
