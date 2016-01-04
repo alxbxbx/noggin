2c169f34468f386d32eb68bbbc97179e2da36d89
Filip Bekic <filip1994sm@gmail.com>
2016-01-04 19:46:46 +0100

    Search layout is completed!

--- src/main/webapp/resources/js/controllers/SearchController.js
+++ src/main/webapp/resources/js/controllers/SearchController.js
@@ -2,12 +2,56 @@
 
 app.controller('SearchController', ['$scope', 'bookFactory', 'categoryFactory', function ($scope, bookFactory, categoryFactory) {
 
+    // Get Books
     bookFactory.getAll().success(function (data) {
         $scope.books = data;
     });
 
+    // Get Categories
     categoryFactory.getAll().success(function (data) {
         $scope.categories = data;
     });
+
+    // Define Conditions
+    $scope.conditions = [
+        {
+            id: 0,
+            name: "must"
+        },
+        {
+            id: 1,
+            name: "should"
+        },
+        {
+            id: 2,
+            name: "mustNot"
+        }
+    ];
+
+    // Set Default Conditions
+    $scope.searchKeywordsCondition = $scope.conditions[0];
+    $scope.searchTitleCondition = $scope.conditions[0];
+    $scope.searchTextCondition = $scope.conditions[0];
+
+    // Define Types
+    $scope.types = [
+        {
+            id: 0,
+            name: "regular"
+        },
+        {
+            id: 1,
+            name: "range"
+        },
+        {
+            id: 2,
+            name: "fuzzy"
+        }
+    ];
+
+    // Set Default Types
+    $scope.searchKeywordsType = $scope.types[0];
+    $scope.searchTitleType = $scope.types[0];
+    $scope.searchTextType = $scope.types[0];
 
 }]);
